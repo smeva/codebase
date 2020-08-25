a3cd5331bfaf501577dbabc136bea696a5e78f77
saswati <saswatimishra.08@gmail.com>
2020-08-26 02:12:46 +0530

    first spring boot project

---
+++ src/main/java/com/hp/app/HelloController.java
@@ -0,0 +1,14 @@
+package com.hp.app;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/hello")
+	public String hi() {
+		return "My First SpringBoot App";
+
+	}
+
+}
